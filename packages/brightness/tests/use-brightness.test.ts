70d35088d61f4d0c9c3526b7a0ac4b57311cdac9
Cedric van Putten <me@bycedric.com>
2020-01-12 21:42:07 +0000

    refactor(brightness): update deps, use bob and rewrite tests

--- packages/brightness/tests/use-brightness.test.ts
+++ packages/brightness/tests/use-brightness.test.ts
@@ -2,49 +2,40 @@ import { renderHook, act } from '@testing-library/react-hooks';
 import * as Brightness from 'expo-brightness';
 import { useBrightness } from '../src/use-brightness';
 
-jest.mock('expo-brightness');
-
 const DATA = 0
 const SET = 1;
 const GET = 2;
 
-test('returns state, set and get callbacks when mounted', async () => {
-	(Brightness.getBrightnessAsync as jest.Mock).mockResolvedValue(1);
+it('returns data, set and get callbacks when mounted', async () => {
+	const hook = renderHook(() => useBrightness({ get: false }));
 
-	const hook = renderHook(useBrightness);
-	await hook.waitForNextUpdate();
-
-	// expect(hook.result.current[DATA]).toBe(1);
+	expect(hook.result.current[DATA]).toBeUndefined();
 	expect(hook.result.current[SET]).toBeInstanceOf(Function);
 	expect(hook.result.current[GET]).toBeInstanceOf(Function);
 });
 
-test('handles state with set callback', async () => {
-	(Brightness.getBrightnessAsync as jest.Mock).mockResolvedValue(1);
-	(Brightness.setBrightnessAsync as jest.Mock).mockResolvedValue(undefined);
+it('updates data with set callback', async () => {
+	const setter = jest.spyOn(Brightness, 'setBrightnessAsync').mockResolvedValue();
 
-	const hook = renderHook(useBrightness);
-	act(() => { hook.result.current[SET](0.5) });
-	await hook.waitForNextUpdate();
+	const hook = renderHook(() => useBrightness({ get: false }));
+	await act(() => hook.result.current[SET](0.5));
 
-	expect(Brightness.setBrightnessAsync).toBeCalledWith(0.5);
+	expect(setter).toBeCalledWith(0.5);
 	expect(hook.result.current[DATA]).toBe(0.5);
 });
 
-test('handles state with get callback', async () => {
-	(Brightness.getBrightnessAsync as jest.Mock)
+it('updates data with get callback', async () => {
+	const getter = jest.spyOn(Brightness, 'getBrightnessAsync')
 		.mockResolvedValueOnce(1)
 		.mockResolvedValueOnce(0.5);
 
-	const hook = renderHook(useBrightness);
-	act(() => { hook.result.current[GET]() });
+	const hook = renderHook(() => useBrightness());
 	await hook.waitForNextUpdate();
 
-	expect(Brightness.getBrightnessAsync).toBeCalledTimes(2);
-	expect(hook.result.current[DATA]).toBe(0.5);
-});
+	expect(hook.result.current[DATA]).toBe(1);
 
-test('gets is skipped', () => {
-	renderHook(() => useBrightness({ get: false }));
-	expect(Brightness.getBrightnessAsync).not.toBeCalled();
+	await act(() => hook.result.current[GET]());
+
+	expect(getter).toBeCalledTimes(2);
+	expect(hook.result.current[DATA]).toBe(0.5);
 });
--- packages/brightness/tests/use-system-brightness-mode.test.ts
+++ packages/brightness/tests/use-system-brightness-mode.test.ts
@@ -2,49 +2,40 @@ import { renderHook, act } from '@testing-library/react-hooks';
 import * as Brightness from 'expo-brightness';
 import { useSystemBrightnessMode } from '../src/use-system-brightness-mode';
 
-jest.mock('expo-brightness');
-
 const DATA = 0
 const SET = 1;
 const GET = 2;
 
-test('returns state, set and get callbacks when mounted', async () => {
-	(Brightness.getSystemBrightnessModeAsync as jest.Mock).mockResolvedValue(Brightness.BrightnessMode.MANUAL);
+it('returns data, set and get callbacks when mounted', async () => {
+	const hook = renderHook(() => useSystemBrightnessMode({ get: false }));
 
-	const hook = renderHook(useSystemBrightnessMode);
-	await hook.waitForNextUpdate();
-
-	expect(hook.result.current[DATA]).toBe(Brightness.BrightnessMode.MANUAL);
+	expect(hook.result.current[DATA]).toBeUndefined();
 	expect(hook.result.current[SET]).toBeInstanceOf(Function);
 	expect(hook.result.current[GET]).toBeInstanceOf(Function);
 });
 
-test('handles state with set callback', async () => {
-	(Brightness.getSystemBrightnessModeAsync as jest.Mock).mockResolvedValue(Brightness.BrightnessMode.MANUAL);
-	(Brightness.setSystemBrightnessModeAsync as jest.Mock).mockResolvedValue(undefined);
+it('updates data with set callback', async () => {
+	const setter = jest.spyOn(Brightness, 'setSystemBrightnessModeAsync').mockResolvedValue();
 
-	const hook = renderHook(useSystemBrightnessMode);
-	act(() => { hook.result.current[SET](Brightness.BrightnessMode.AUTOMATIC) });
-	await hook.waitForNextUpdate();
+	const hook = renderHook(() => useSystemBrightnessMode({ get: false }));
+	await act(() => hook.result.current[SET](Brightness.BrightnessMode.AUTOMATIC));
 
-	expect(Brightness.setSystemBrightnessModeAsync).toBeCalledWith(Brightness.BrightnessMode.AUTOMATIC);
+	expect(setter).toBeCalledWith(Brightness.BrightnessMode.AUTOMATIC);
 	expect(hook.result.current[DATA]).toBe(Brightness.BrightnessMode.AUTOMATIC);
 });
 
-test('handles state with get callback', async () => {
-	(Brightness.getSystemBrightnessModeAsync as jest.Mock)
+it('updates data with get callback', async () => {
+	const getter = jest.spyOn(Brightness, 'getSystemBrightnessModeAsync')
 		.mockResolvedValueOnce(Brightness.BrightnessMode.AUTOMATIC)
 		.mockResolvedValueOnce(Brightness.BrightnessMode.MANUAL);
 
 	const hook = renderHook(useSystemBrightnessMode);
-	act(() => { hook.result.current[GET]() });
 	await hook.waitForNextUpdate();
 
-	expect(Brightness.getSystemBrightnessModeAsync).toBeCalledTimes(2);
-	expect(hook.result.current[DATA]).toBe(Brightness.BrightnessMode.MANUAL);
-});
+	expect(hook.result.current[DATA]).toBe(Brightness.BrightnessMode.AUTOMATIC);
 
-test('gets is skipped', () => {
-	renderHook(() => useSystemBrightnessMode({ get: false }));
-	expect(Brightness.getSystemBrightnessModeAsync).not.toBeCalled();
+	await act(() => hook.result.current[GET]());
+
+	expect(getter).toBeCalledTimes(2);
+	expect(hook.result.current[DATA]).toBe(Brightness.BrightnessMode.MANUAL);
 });
