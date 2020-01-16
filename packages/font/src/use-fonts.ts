267b94e8e6ba2b6d8ce2af6221c187574216c5d2
Cedric van Putten <me@bycedric.com>
2020-01-16 20:54:26 +0000

    docs(font): add docs link, example and info about hook

--- packages/font/src/use-fonts.ts
+++ packages/font/src/use-fonts.ts
@@ -2,6 +2,15 @@ import { useEffect, useState } from 'react';
 import { Asset } from 'expo-asset';
 import { loadAsync } from 'expo-font';
 
+/**
+ * Load a map of custom fonts to use in textual elements.
+ * The map keys are used as font names, and can be used with `fontFamily: <name>;`.
+ * It returns a boolean stating if all fonts are loaded.
+ *
+ * @see https://docs.expo.io/versions/latest/sdk/font/
+ * @example
+ * const [loaded] = useFonts({ ... });
+ */
 export function useFonts(map: FontMap): UseFontsSignature {
 	const [loaded, setLoaded] = useState(false);
 
