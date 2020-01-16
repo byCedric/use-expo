e461dccd44d5c6b8417627b27cc2b1d18b2e7387
Cedric van Putten <me@bycedric.com>
2020-01-16 01:16:06 +0100

    docs(battery): use code annotation in state enum (#57)

--- packages/battery/src/use-battery-state.ts
+++ packages/battery/src/use-battery-state.ts
@@ -4,10 +4,10 @@ import { BatteryState, getBatteryStateAsync, addBatteryStateListener } from 'exp
 /**
  * Get or track the battery state of the device.
  * It returns the `BatteryState` enum with one of the following values:
- *   - 0 (UNKNOWN), if the battery state is unknown or unable to access
- *   - 1 (UNPLUGGED), if the battery is not charging or discharging
- *   - 2 (CHARGING), if the battery is charging
- *   - 3 (FULL), if the battery level is full
+ *   - 0 `UNKNOWN` if the battery state is unknown or unable to access
+ *   - 1 `UNPLUGGED` if the battery is not charging or discharging
+ *   - 2 `CHARGING` if the battery is charging
+ *   - 3 `FULL` if the battery level is full
  *
  * @see https://docs.expo.io/versions/latest/sdk/battery/#batterygetbatterystateasync
  */
