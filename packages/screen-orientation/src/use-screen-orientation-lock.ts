108fc5f040250c7028ccdcbfe09bc40fd0ab3a72
Cedric van Putten <me@bycedric.com>
2020-01-18 17:58:08 +0100

    docs(screen-orientation): add information about the hooks (#61)

--- packages/screen-orientation/src/use-screen-orientation-lock.ts
+++ packages/screen-orientation/src/use-screen-orientation-lock.ts
@@ -1,6 +1,15 @@
 import { useEffect } from 'react';
 import { ScreenOrientation } from 'expo';
 
+/**
+ * Lock the screen orientation of the device to a lock mode.
+ * It accepts any value from the `OrientationLock` enum and unlocks the screen when unmounted.
+ * This hook is similar to the `useKeepAwake` hook and does not return anything.
+ *
+ * @see https://docs.expo.io/versions/latest/sdk/screen-orientation/
+ * @example
+ * useScreenOrientationLock(...);
+ */
 export function useScreenOrientationLock(orientation: ScreenOrientation.OrientationLock): void {
 	useEffect(() => {
 		ScreenOrientation.lockAsync(orientation);
