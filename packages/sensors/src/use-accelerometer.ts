b57b8bface2a5ebac9d3179dd58113971fa3fdbb
Cedric van Putten <me@bycedric.com>
2020-04-04 15:57:22 +0000

    refactor(sensors): upgrade to expo sdk 37

--- packages/sensors/src/use-accelerometer.ts
+++ packages/sensors/src/use-accelerometer.ts
@@ -33,7 +33,7 @@ export function useAccelerometer(
 		}
 
 		return Accelerometer.addListener(setData).remove;
-	}, [available, availability, interval]);
+	}, [availability, interval]);
 
 	return [data, available];
 }
