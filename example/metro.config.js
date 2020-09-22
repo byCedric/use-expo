99d6ff9ac2c943e5993690c69d9e524ec182e0fb
Cedric van Putten <me@bycedric.com>
2020-09-22 15:05:38 +0200

    chore: fully upgrade to SDK 39

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,8 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { createMetroConfiguration } = require('expo-yarn-workspaces');
 
-const config = createMetroConfiguration(__dirname);
-
-console.log(config);
-
-module.exports = config;
+module.exports = createMetroConfiguration(__dirname);
